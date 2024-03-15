281c7891536d76edd13bbab0542c8ef13473f1cb
ruda8711 <127097067+ruda8711@users.noreply.github.com>
2024-03-15 11:32:45 +0900

    init

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.0'
+
+target 'testui' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for testui
+  pod 'Alamofire', '~> 5.6.2'
+
+end
---
+++ testui/testui.h
@@ -0,0 +1,18 @@
+//
+//  testui.h
+//  testui
+//
+//  Created by Momo Jo on 3/15/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testui.
+FOUNDATION_EXPORT double testuiVersionNumber;
+
+//! Project version string for testui.
+FOUNDATION_EXPORT const unsigned char testuiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testui/PublicHeader.h>
+
+
